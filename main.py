8f3decb74017f5b1b81fe4d66034329f6ab95472
paceydiep <162515875+paceydiep@users.noreply.github.com>
2026-06-03 09:33:58 -0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+check4 = False
+check3 = False
+check2 = False
+check1 = False
+while check1 == False or check2 == False or check3 == False or check4 == False:
+    if input.button_is_pressed(Button.A):
+        basic.show_icon(IconNames.HEART)
+        check1 = True
+    elif input.button_is_pressed(Button.B):
+        basic.show_icon(IconNames.TARGET)
+        check2 = True
+    elif input.compass_heading() == 180:
+        basic.show_icon(IconNames.BUTTERFLY)
+        check3 = True
+    elif input.is_gesture(Gesture.SHAKE):
+        basic.show_icon(IconNames.FABULOUS)
+        check4 = True
+basic.show_string("Well Done!")
--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-})
+let check4 = false
+let check3 = false
+let check2 = false
+let check1 = false
+while (check1 == false || check2 == false || check3 == false || check4 == false) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Heart)
+        check1 = true
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Target)
+        check2 = true
+    } else if (input.compassHeading() == 180) {
+        basic.showIcon(IconNames.Butterfly)
+        check3 = true
+    } else if (input.isGesture(Gesture.Shake)) {
+        basic.showIcon(IconNames.Fabulous)
+        check4 = true
+    }
+}
+basic.clearScreen()
+basic.pause(2000)
+basic.showString("Well Done!")
