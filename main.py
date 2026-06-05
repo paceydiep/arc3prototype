815540b1e6b8d1b3f6f462be8c0df16defd1680e
paceydiep <162515875+paceydiep@users.noreply.github.com>
2026-06-05 09:44:59 -0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ check4 = False
 check3 = False
 check2 = False
 check1 = False
+basic.show_string("Try to input the correct sequence")
 while check1 == False or check2 == False or check3 == False or check4 == False:
-    if input.button_is_pressed(Button.A):
-        basic.show_icon(IconNames.HEART)
-        check1 = True
-    elif input.button_is_pressed(Button.B):
-        basic.show_icon(IconNames.TARGET)
-        check2 = True
-    elif input.compass_heading() == 180:
-        basic.show_icon(IconNames.BUTTERFLY)
-        check3 = True
-    elif input.is_gesture(Gesture.SHAKE):
-        basic.show_icon(IconNames.FABULOUS)
-        check4 = True
+    if not (input.running_time() == 45000):
+        if input.button_is_pressed(Button.A):
+            basic.show_icon(IconNames.HEART)
+            check1 = True
+        elif input.button_is_pressed(Button.B):
+            basic.show_icon(IconNames.TARGET)
+            check2 = True
+        elif input.compass_heading() == 180:
+            basic.show_icon(IconNames.BUTTERFLY)
+            check3 = True
+        elif input.is_gesture(Gesture.SHAKE):
+            basic.show_icon(IconNames.FABULOUS)
+            check4 = True
+    else:
+        basic.clear_screen()
+        basic.pause(2000)
+        music.play(music.string_playable("C5 A B G A F G E ", 120),
+            music.PlaybackMode.IN_BACKGROUND)
+        basic.show_string("Time's Up!")
+basic.clear_screen()
+basic.pause(2000)
+music.play(music.string_playable("G B A G C5 B A B ", 120),
+    music.PlaybackMode.IN_BACKGROUND)
 basic.show_string("Well Done!")
--- main.ts
+++ main.ts
@@ -2,21 +2,34 @@ let check4 = false
 let check3 = false
 let check2 = false
 let check1 = false
+basic.showString("Try to input the correct sequence")
+let step = 1
 while (check1 == false || check2 == false || check3 == false || check4 == false) {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showIcon(IconNames.Heart)
-        check1 = true
-    } else if (input.buttonIsPressed(Button.B)) {
-        basic.showIcon(IconNames.Target)
-        check2 = true
-    } else if (input.compassHeading() == 180) {
-        basic.showIcon(IconNames.Butterfly)
-        check3 = true
-    } else if (input.isGesture(Gesture.Shake)) {
-        basic.showIcon(IconNames.Fabulous)
-        check4 = true
+    if (!(input.runningTime() == 45000)) {
+        if (input.buttonIsPressed(Button.A) && step == 1) {
+            basic.showIcon(IconNames.Heart)
+            check1 = true
+            step = 2
+        } else if (input.buttonIsPressed(Button.B) && step == 2) {
+            basic.showIcon(IconNames.Target)
+            check2 = true
+            step = 3
+        } else if (input.compassHeading() == 180 && step == 3) {
+            basic.showIcon(IconNames.Butterfly)
+            check3 = true
+            step = 4
+        } else if (input.isGesture(Gesture.Shake) && step == 4) {
+            basic.showIcon(IconNames.Fabulous)
+            check4 = true
+        }
+    } else {
+        basic.clearScreen()
+        basic.pause(2000)
+        music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.InBackground)
+        basic.showString("Time's Up!")
     }
 }
 basic.clearScreen()
 basic.pause(2000)
+music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
 basic.showString("Well Done!")
