e64de80237da0dfaa64d7a76799aea6ce3908636
Charles Gao <165427926+charlesgao5199@users.noreply.github.com>
2026-06-08 15:14:18 -0700

    Final

--- main.py
+++ main.py
@@ -1,30 +1,71 @@
-check4 = False
-check3 = False
-check2 = False
-check1 = False
-basic.show_string("Try to input the correct sequence")
-while check1 == False or check2 == False or check3 == False or check4 == False:
-    if not (input.running_time() == 45000):
-        if input.button_is_pressed(Button.A):
-            basic.show_icon(IconNames.HEART)
-            check1 = True
-        elif input.button_is_pressed(Button.B):
-            basic.show_icon(IconNames.TARGET)
-            check2 = True
-        elif input.compass_heading() == 180:
-            basic.show_icon(IconNames.BUTTERFLY)
-            check3 = True
-        elif input.is_gesture(Gesture.SHAKE):
-            basic.show_icon(IconNames.FABULOUS)
-            check4 = True
-    else:
-        basic.clear_screen()
-        basic.pause(2000)
-        music.play(music.string_playable("C5 A B G A F G E ", 120),
-            music.PlaybackMode.IN_BACKGROUND)
-        basic.show_string("Time's Up!")
+step = 0
+was_at_180 = False
+
+def try_again():
+    basic.clear_screen()
+    basic.show_string("Try Again")
+    basic.clear_screen()
+
+def correct_action(icon):
+    basic.show_icon(icon)
+    basic.pause(500)
+    basic.clear_screen()
+
+while step < 4:
+
+    current_heading_is_180 = input.compass_heading() == 180
+
+    # Step 1: Press A
+    if input.button_is_pressed(Button.A):
+        if step == 0:
+            correct_action(IconNames.HEART)
+            step = 1
+        else:
+            try_again()
+            step = 0
+
+        while input.button_is_pressed(Button.A):
+            basic.pause(50)
+
+    # Step 2: Press B
+    elif input.button_is_pressed(Button.B):
+        if step == 1:
+            correct_action(IconNames.DIAMOND)
+            step = 2
+        else:
+            try_again()
+            step = 0
+
+        while input.button_is_pressed(Button.B):
+            basic.pause(50)
+
+    # Rotate to 180 degrees
+    # This now works as an input at ANY step
+    elif current_heading_is_180 and was_at_180 == False:
+        if step == 2:
+            correct_action(IconNames.NO)
+            step = 3
+        else:
+            try_again()
+            step = 0
+
+        was_at_180 = True
+
+    # Step 4: Shake
+    elif input.is_gesture(Gesture.SHAKE):
+        if step == 3:
+            correct_action(IconNames.CHESSBOARD)
+            step = 4
+        else:
+            try_again()
+            step = 0
+
+        basic.pause(800)
+
+    # Reset rotate detection after leaving 180 degrees
+    if current_heading_is_180 == False:
+        was_at_180 = False
+
 basic.clear_screen()
-basic.pause(2000)
-music.play(music.string_playable("G B A G C5 B A B ", 120),
-    music.PlaybackMode.IN_BACKGROUND)
+basic.pause(1000)
 basic.show_string("Well Done!")
--- main.ts
+++ main.ts
@@ -1,33 +1,77 @@
-let check4 = false
-let check3 = false
-let check2 = false
-let check1 = false
-basic.showString("Try to input the correct sequence")
-let step = 1
-while (check1 == false || check2 == false || check3 == false || check4 == false) {
-    if (!(input.runningTime() == 45000)) {
-        if (input.buttonIsPressed(Button.A) && step == 1) {
-            basic.showIcon(IconNames.Heart)
-            check1 = true
-            step = 2
-        } else if (input.buttonIsPressed(Button.B) && step == 2) {
-            basic.showIcon(IconNames.Target)
-            check2 = true
-            step = 3
-        } else if (input.compassHeading() == 180 && step == 3) {
-            basic.showIcon(IconNames.Butterfly)
-            check3 = true
-            step = 4
-        } else if (input.isGesture(Gesture.Shake) && step == 4) {
-            basic.showIcon(IconNames.Fabulous)
-            check4 = true
+let current_heading_is_180: any;
+let step = 0
+let was_at_180 = false
+function try_again() {
+    basic.clearScreen()
+    basic.showString("Try Again")
+    basic.clearScreen()
+}
+
+function correct_action(icon: number) {
+    basic.showIcon(icon)
+    basic.pause(500)
+    basic.clearScreen()
+}
+
+while (step < 4) {
+    current_heading_is_180 = input.compassHeading() == 180
+    //  Step 1: Press A
+    if (input.buttonIsPressed(Button.A)) {
+        if (step == 0) {
+            correct_action(IconNames.Heart)
+            step = 1
+        } else {
+            try_again()
+            step = 0
         }
-    } else {
-        basic.clearScreen()
-        basic.pause(2000)
-        basic.showString("Time's Up!")
+        
+        while (input.buttonIsPressed(Button.A)) {
+            basic.pause(50)
+        }
+    } else if (input.buttonIsPressed(Button.B)) {
+        //  Step 2: Press B
+        if (step == 1) {
+            correct_action(IconNames.Diamond)
+            step = 2
+        } else {
+            try_again()
+            step = 0
+        }
+        
+        while (input.buttonIsPressed(Button.B)) {
+            basic.pause(50)
+        }
+    } else if (current_heading_is_180 && was_at_180 == false) {
+        //  Rotate to 180 degrees
+        //  This now works as an input at ANY step
+        if (step == 2) {
+            correct_action(IconNames.No)
+            step = 3
+        } else {
+            try_again()
+            step = 0
+        }
+        
+        was_at_180 = true
+    } else if (input.isGesture(Gesture.Shake)) {
+        //  Step 4: Shake
+        if (step == 3) {
+            correct_action(IconNames.Chessboard)
+            step = 4
+        } else {
+            try_again()
+            step = 0
+        }
+        
+        basic.pause(800)
     }
+    
+    //  Reset rotate detection after leaving 180 degrees
+    if (current_heading_is_180 == false) {
+        was_at_180 = false
+    }
+    
 }
 basic.clearScreen()
-basic.pause(2000)
+basic.pause(1000)
 basic.showString("Well Done!")
